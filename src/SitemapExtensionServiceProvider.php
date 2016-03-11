a267493b379fd4f936148a2486124fbfe64154df
Ryan Thompson <ryan@pyrocms.com>
2016-03-11 10:17:23 -0600

    Info

--- src/SitemapExtensionServiceProvider.php
+++ src/SitemapExtensionServiceProvider.php
@@ -2,35 +2,38 @@
 
 use Anomaly\Streams\Platform\Addon\AddonServiceProvider;
 
+/**
+ * Class SitemapExtensionServiceProvider
+ *
+ * @link          http://pyrocms.com/
+ * @author        PyroCMS, Inc. <support@pyrocms.com>
+ * @author        Ryan Thompson <ryan@pyrocms.com>
+ * @package       Anomaly\SitemapExtension
+ */
 class SitemapExtensionServiceProvider extends AddonServiceProvider
 {
 
-    protected $plugins = [];
+    /**
+     * The addon routes.
+     *
+     * @var array
+     */
+    protected $routes = [
+        //'sitemap.xml' => ''
+    ];
 
-    protected $routes = [];
-
-    protected $middleware = [];
-
-    protected $listeners = [];
-
-    protected $aliases = [];
-
-    protected $bindings = [];
-
+    /**
+     * The addon providers.
+     *
+     * @var array
+     */
     protected $providers = [];
 
+    /**
+     * The addon singletons.
+     *
+     * @var array
+     */
     protected $singletons = [];
-
-    protected $overrides = [];
-
-    protected $mobile = [];
-
-    public function register()
-    {
-    }
-
-    public function map()
-    {
-    }
 
 }
