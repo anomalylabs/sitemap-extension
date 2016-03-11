c768fda9a12fe4f4951f767a2d0619496aab9aeb
Ryan Thompson <ryan@pyrocms.com>
2016-03-11 10:07:06 -0600

    Initial commit

---
+++ src/SitemapExtensionServiceProvider.php
@@ -0,0 +1,36 @@
+<?php namespace Anomaly\SitemapExtension;
+
+use Anomaly\Streams\Platform\Addon\AddonServiceProvider;
+
+class SitemapExtensionServiceProvider extends AddonServiceProvider
+{
+
+    protected $plugins = [];
+
+    protected $routes = [];
+
+    protected $middleware = [];
+
+    protected $listeners = [];
+
+    protected $aliases = [];
+
+    protected $bindings = [];
+
+    protected $providers = [];
+
+    protected $singletons = [];
+
+    protected $overrides = [];
+
+    protected $mobile = [];
+
+    public function register()
+    {
+    }
+
+    public function map()
+    {
+    }
+
+}
